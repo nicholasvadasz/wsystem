c56d8012b349044ae32aa1c6b9dd4ed5a6411213
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2022-12-11 11:37:20 -0500

    without build

--- build/CMakeFiles/lab08.dir/src/Settings.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lab08.dir/src/Settings.cpp.o: \
-  /Users/nick/GitHub/fun/lab08-trimeshes-nicholasvadasz/src/Settings.cpp \
-  /Users/nick/GitHub/fun/lab08-trimeshes-nicholasvadasz/src/Settings.h
--- build/CMakeFiles/lab08_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for lab08_autogen.
-# This may be replaced when dependencies are built.
