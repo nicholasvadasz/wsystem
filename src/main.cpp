0fd73f7adea3aa5be19c5d04866b80644654f18a
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2022-12-10 22:28:26 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,38 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QScreen>
 #include <QSurfaceFormat>
+#include <QScreen>
 
-int main(int argc, char *argv[]) {
-  // Setting up the application
-  QApplication a(argc, argv);
+int main(int argc, char *argv[])
+{
+    // Setting up the application
+    QApplication a(argc, argv);
 
-  QCoreApplication::setApplicationName("Crystal Geometry Generator");
-  QCoreApplication::setOrganizationName("QtProject");
-  QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCoreApplication::setApplicationName("Crystal Generation via L-Systems");
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
-  // Format for QSurface (a renderable surface in Qt)
-  QSurfaceFormat fmt;
-  fmt.setDepthBufferSize(24);
-  fmt.setVersion(4, 1);
-  fmt.setProfile(QSurfaceFormat::CoreProfile);
-  QSurfaceFormat::setDefaultFormat(fmt);
+    // Format for QSurface (a renderable surface in Qt)
+    QSurfaceFormat fmt;
+    fmt.setDepthBufferSize(24);
+    fmt.setVersion(4, 1);
+    fmt.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(fmt);
 
-  // Create a main window. See mainwindow.h/cpp for details. Contains a GLWidget
-  // (see glwidget.h/cpp for details)
-  MainWindow w;
-  w.setupUI();
-  w.resize(650, 400);
-  w.setWindowTitle(QStringLiteral("Crystal Geometry Generator"));
-  int desktopArea = QGuiApplication::primaryScreen()->size().width() *
-                    QGuiApplication::primaryScreen()->size().height();
-  int widgetArea = w.width() * w.height();
-  if (((float)widgetArea / (float)desktopArea) < 0.75f) {
-    w.show();
-  } else {
-    w.showMaximized();
-  }
+    // Create a main window. See mainwindow.h/cpp for details. Contains a GLWidget (see glwidget.h/cpp for details)
+    MainWindow w;
+    w.setupUI();
+    w.resize(650, 400);
+    w.setWindowTitle(QStringLiteral("W Systems: Crystal Generation via L-Systems"));
+    int desktopArea = QGuiApplication::primaryScreen()->size().width() *
+                      QGuiApplication::primaryScreen()->size().height();
+    int widgetArea = w.width() * w.height();
+    if (((float)widgetArea / (float)desktopArea) < 0.75f) {
+        w.show();
+    } else {
+        w.showMaximized();
+    }
 
-  return a.exec();
+    return a.exec();
 }
